cca06d4d8a4d31f8e4e6c0d1a42712f08d085f21
Dmytro Golub <dmytro.golub@gmail.com>
2025-05-28 10:13:17 +0100

    Initial Commit

---
+++ HomeInventory/HomeInventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeInventoryApp.swift
+//  HomeInventory
+//
+//  Created by Dmytro Golub on 28/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct HomeInventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
